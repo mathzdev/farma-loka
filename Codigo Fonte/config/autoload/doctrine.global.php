58f84dec8d6a7b5c77ed9a1679f903842042a772
Matheus Lucio <matheusluciox@gmail.com>
2015-10-28 12:54:11 -0200

    Adicionando esqueci minha senha e meus dados

--- Codigo Fonte/config/autoload/doctrine.global.php
+++ Codigo Fonte/config/autoload/doctrine.global.php
@@ -8,7 +8,7 @@ return array(
                     'host'     => '127.0.0.1',
                     'port'     => '3306',
                     'user'     => 'root',
-                    'password' => '',
+                    'password' => 'root',
                     'dbname'   => 'farmaloka',
                 )
             )
